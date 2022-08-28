d147d2000086a088a6fed039632d189150d6c3a3
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-08-28 13:22:55 +0100

    Update interpreter.py

--- python_plus/interpreter.py
+++ python_plus/interpreter.py
@@ -1,3 +1,5 @@
+import argparse
+
 def convert(s):
     r = ''
     for l in s.splitlines():
@@ -65,3 +67,17 @@ def convert(s):
                 check = True
         r += colon * ':' + '\n'
     return r
+
+def from_file():
+    parser = argparse.ArgumentParser(prog ='pythonplus',
+                                     description ='Run a Python+ file using the pythonplus command')
+    parser.add_argument('filename', metavar ='filename', type=str, nargs=1, help= 'The filename of the file to run using the Python+ Interpreter')
+    args = parser.parse_args()
+    try:
+        with open(args.filename[0]) as f:
+            exec(convert(f.read()))
+            return 0
+    except:
+        pass
+    print('An error occurred')
+    return 1
