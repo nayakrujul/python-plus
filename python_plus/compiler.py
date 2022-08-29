4a600a12a708984a3bbe90f01e088ecb40a89018
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-08-29 11:07:20 +0100

    Update compiler.py

--- python_plus/compiler.py
+++ python_plus/compiler.py
@@ -72,7 +72,7 @@ def from_file():
     parser = argparse.ArgumentParser(prog ='pythonplus',
                                      description ='Run a Python+ file using the pythonplus command')
     parser.add_argument('filename', metavar ='filename', type=str, nargs=1, help= 'The filename of the file to run using the Python+ Compiler')
-    parser.add_argument('-d', metavar='debug', type=bool, nargs='?', const=False, help= 'Debug the program by showing the converted Python code?')
+    parser.add_argument('-d', dest='debug', type=bool, nargs='?', const=False, help= 'Debug the program by showing the converted Python code?')
     args = parser.parse_args()
     try:
         with open(args.filename[0]) as f:
