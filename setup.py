b11d9db29d57e7da761a8ebcbe930635cbb4043e
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-08-29 11:00:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = 'Shorten your Python code with shorter syntax - read the docs
 
 setup(
   name = 'shorter-python',
-  version = '4.0',
+  version = '4.1',
   license='Apache',
   description = 'Shorten your Python code with shorter syntax.',
   author = 'Rujul Nayak',
